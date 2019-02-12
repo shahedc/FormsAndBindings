1e58d08f43898ec03c6b8428eec79417831fd625
shahedc <shahedc@gmail.com>
2019-02-12 00:37:59 -0500

    Added MVC attributes for model binding
    Added MVC attributes for model binding

---
+++ WebMvc/Controllers/AttrController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormsAndBindings.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebMvc.Controllers
+{
+    public class AttrController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index([FromQuery] string humanInfo)
+        {
+            ViewData["humanInfo"] = humanInfo;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details([FromRoute] int id)
+        {
+            Human human = new Human
+            {
+                Id = id,
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "123 ABC St",
+                DateOfBirth = DateTime.Now,
+                FavoriteNumber = 7,
+                IsActive = true
+            };
+
+            return View(human);
+        }
+
+        [HttpPost]
+        public IActionResult Details([FromForm] Human human)
+        {
+            var humanInfo = new StringBuilder();
+            humanInfo.Append(human.FirstName + " " + human.LastName);
+            humanInfo.Append(", Date of Birth: " + human.DateOfBirth.ToString("yyyy-MM-dd"));
+            humanInfo.Append(", Id: " + human.Id + ", Favorite Number: " + human.FavoriteNumber);
+            return RedirectToAction("index", new { humanInfo = humanInfo.ToString() });
+        }
+    }
+}
